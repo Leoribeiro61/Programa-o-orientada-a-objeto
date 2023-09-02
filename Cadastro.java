6a1fd901b772bd7a47eb85bc5d8fd8c7d171a580
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2023-09-01 21:18:16 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,29 @@
+
+public class Cadastro {
+    //atributos
+    public String chassi;
+    public String placa;
+    public String cor;
+    public String modelo;
+    public int ano;
+    
+    /*public Cadastro (){
+        
+    }*/
+    //metodo construtor com argumentos
+    //public Cadastro(int ano){
+        //this.ano = ano;
+    //}
+    
+    
+    public Cadastro(String chassi, String placa, String cor, String modelo, int ano){
+       
+        //implementação 
+        this.chassi = chassi;
+        this.placa = placa;
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ano = ano;
+        
+    }
+}
