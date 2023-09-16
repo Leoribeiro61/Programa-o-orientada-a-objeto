f03a77e35f93ffa22b0828e3c2403161763eff3e
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2023-09-15 21:32:26 -0300

    Add files via upload

---
+++ ContaPoupanca.java
@@ -0,0 +1,8 @@
+
+public class ContaPoupanca extends Conta{
+    
+    public ContaPoupanca(String nome, String cpf, String telefone, String conta, String agencia) {
+        super(nome, cpf, telefone, conta, agencia);
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -3,13 +3,18 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Conta c1 = new Conta("pedro", "123", "456", "001", "23456-0");
         
-        System.out.println("Impressao: "+c1.toString());
+        //Conta c1 = new Conta("Pedro", "123", "456", "001", "23456-0");
         
-        Cliente cliente = new Cliente("Maria", "678", "444");
+        ContaPoupanca cp = new ContaPoupanca("Lucas", "000", "1010", "002", "78910-0");
         
-        System.out.println("Cliente "+cliente.toString());
+        //System.out.println("Impressao: "+c1.toString());
+        
+        //Cliente cliente = new Cliente("Maria", "678", "444");
+        
+        //System.out.println("Cliente "+cliente.toString());
+        
+        System.out.println(" "+cp.toString());
     }
     
 }
